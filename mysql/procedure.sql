63bf55ffefb728d692b06a8d67f7770ef564c6e7
李国玮 <20373159@buaa.edu.cn>
2022-10-22 20:36:58 +0800

    10.22

--- mysql/procedure.sql
+++ mysql/procedure.sql
@@ -2,7 +2,7 @@
 delimiter $$
 drop procedure addcomment;
 create procedure addcomment(in pTitle_id int, in pContent varchar(10000),
-                            in pUsername varchar(100), in pTime datetime)
+                            in pUsername varchar(100), in pTime datetime, in pCommentatee_name varchar(50))
 begin
     declare vComment_id int;
     -- 当发生错误时, error会被自动记为1
@@ -10,7 +10,7 @@ begin
     declare continue handler for sqlexception set error=1;
     -- 开始事务, 保证要么全体成功, 要么全体失败
     start transaction ;
-        insert into dc_comment(content, time, username) values (pContent, pTime, pUsername);
+        insert into dc_comment(content, time, username, commentatee_name) values (pContent, pTime, pUsername, pCommentatee_name);
         set vComment_id = (select MAX(id) from dc_comment);
         insert into dc_com2title(title_id, comment_id) values (pTitle_id, vComment_id);
     if error = 1 then
@@ -20,39 +20,39 @@ begin
     end if ;
 end$$
 
-delimiter $$
-drop procedure deletecomment;
-create procedure deletecomment(in pComment_id int)
-begin
-    declare error int default 0;
-    declare continue handler for sqlexception set error=1;
-    start transaction ;
-        delete from dc_com2title where comment_id = pComment_id;
-        delete from dc_comment where id = pComment_id;
-    if error = 1 then
-        rollback ;
-    else
-        commit ;
-    end if ;
-    select error, pComment_id;
-end $$
-delimiter ;
+# delimiter $$
+# drop procedure deletecomment;
+# create procedure deletecomment(in pComment_id int)
+# begin
+#     declare error int default 0;
+#     declare continue handler for sqlexception set error=1;
+#     start transaction ;
+#         delete from dc_com2title where comment_id = pComment_id;
+#         delete from dc_comment where id = pComment_id;
+#     if error = 1 then
+#         rollback ;
+#     else
+#         commit ;
+#     end if ;
+#     select error, pComment_id;
+# end $$
+# delimiter ;
 
 
-delimiter $$
-drop procedure deletetitle;
-create procedure deletetitle(in pTitle_id int)
-begin
-    declare error int default 0;
-    declare continue handler for sqlexception set error=1;
-    start transaction ;
-        delete from dc_com2title where title_id = pTitle_id;
-        delete from dc_title where id = pTitle_id;
-    if error = 1 then
-        rollback ;
-    else
-        commit ;
-    end if ;
-    select error, pTitle_id;
-end $$
-delimiter ;
+# delimiter $$
+# drop procedure deletetitle;
+# create procedure deletetitle(in pTitle_id int)
+# begin
+#     declare error int default 0;
+#     declare continue handler for sqlexception set error=1;
+#     start transaction ;
+#         delete from dc_com2title where title_id = pTitle_id;
+#         delete from dc_title where id = pTitle_id;
+#     if error = 1 then
+#         rollback ;
+#     else
+#         commit ;
+#     end if ;
+#     select error, pTitle_id;
+# end $$
+# delimiter ;
--- mysql/table.sql
+++ mysql/table.sql
@@ -6,7 +6,7 @@ drop table tb_user;
 create table if not exists tb_user(
     username varchar(100) primary key ,
     password varchar(100) not null,
-    user_type INT default 0,
+    user_type varchar(10) default 'stu',
     name varchar(50) default '佚名' not null
 );
 
@@ -20,21 +20,22 @@ create table if not exists dc_title(
     content varchar(10000),
     time datetime,
     username varchar(100),
-    foreign key(username) references tb_user(username)
+    foreign key(username) references tb_user(username) on delete cascade
 );
 
 create table dc_comment(
     id int primary key auto_increment,
     content varchar(10000),
     time datetime,
-    username varchar(100),
-    foreign key (username) references tb_user(username)
+    username varchar(100),#评论人
+    commentatee_name varchar(50),#被评论人姓名
+    foreign key (username) references tb_user(username) on delete cascade
 );
 
 create table dc_com2title(
     title_id int not null ,
     comment_id int not null ,
-    primary key (title_id, comment_id),
-    foreign key (title_id) references dc_title(id),
-    foreign key (comment_id) references dc_comment(id)
+    primary key (comment_id),
+    foreign key (title_id) references dc_title(id) on delete cascade ,
+    foreign key (comment_id) references dc_comment(id) on delete cascade
 );
